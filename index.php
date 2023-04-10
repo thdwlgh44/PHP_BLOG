65516f2adfc67dd8c7669c6ea188074a3496fa93
thdwlgh44 <thdwlgh44@naver.com>
2023-04-10 23:47:18 +0900

    0410 first commit write_process

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once 'bootstrap/app.php';
+
+// $_SESSION['user'] = true;
+
+?>
+
+<?php require_once 'layouts/top.php' ?>
+<?php require_once 'layouts/bottom.php' ?>
---
+++ layouts/bottom.php
@@ -0,0 +1,9 @@
+            </main>
+        </div>    
+        <!-- UIkit JS -->
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.14/dist/js/uikit.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.14/dist/js/uikit-icons.min.js"></script>
+        <script src="https://cdn.ckeditor.com/ckeditor5/16.0.0/balloon-block/ckeditor.js"></script>
+        <script src="/app.js"></script>
+    </body>
+</html>
